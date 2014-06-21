60455bb9ff683b853f01da172b35d2d7352436ae
PrototypeAlex <darth.gibson@gmail.com>
2014-06-22 11:12:59 +1200

    Added catch for slackbot

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -5,7 +5,7 @@ class WebhooksController < ApplicationController
   def incoming
     portal = Portal.where(outgoing_webhook_token: params[:token]).first
 
-    portal.tunnel(params) if portal
+    portal.tunnel(params) if portal && params[:user_id] != 'USLACKBOT'
     render status: 200
   rescue Exception => e
     logger.error params
