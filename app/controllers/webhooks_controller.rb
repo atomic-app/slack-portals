205e2e90697ce6b7c42c29b685c2489b33de9f49
PrototypeAlex <darth.gibson@gmail.com>
2014-06-22 09:58:41 +1200

    added logging

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,6 +3,7 @@ class WebhooksController < ApplicationController
   protect_from_forgery except: :incoming 
 
   def incoming
+    logger.info params
     portal = Portal.where(outgoing_webhook_token: params[:token]).first
 
     portal.tunnel(params) if portal
