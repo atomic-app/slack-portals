a85de8535202faedae835c5db24a59f849dd21b2
PrototypeAlex <darth.gibson@gmail.com>
2014-06-22 11:10:46 +1200

    Added exception handling

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -3,10 +3,12 @@ class WebhooksController < ApplicationController
   protect_from_forgery except: :incoming 
 
   def incoming
-    logger.info params
     portal = Portal.where(outgoing_webhook_token: params[:token]).first
 
     portal.tunnel(params) if portal
+    render status: 200
+  rescue Exception => e
+    logger.error params
     render status: 200
   end
 end
