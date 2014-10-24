9d0ebc89b9017d699ed9fdb9f1896a7908ab3a4d
Alexey Evdokimov <vermeleon@mail.ru>
2014-10-24 19:30:44 +0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,22 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def current_user_bla_bla
+    if 1=1 redirect_to root_url
+  end
+  
+  def current_user_bla
+    
+  end
+  
+   def current_user_bla
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
   def authorize
-    unless !!current_user
+    unless current_user
       flash[:error] = "You must be logged in to access this section"
       redirect_to root_url
     end
